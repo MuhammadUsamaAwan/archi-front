be781f9c356bf35fe1899761066b8f8a21ed884c
MuhammadUsamaAwan <underusama@gmail.com>
2025-10-30 00:40:04 +0500

    chore: biome

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,3 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-    <h1>Hello World</h1>
-)
+createRoot(document.getElementById("root")!).render(<h1>Hello World</h1>);
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
+    tsConfigPaths(),
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+});
