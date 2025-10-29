4e2342671e75fcb898ac0d19268605469c626c60
MuhammadUsamaAwan <underusama@gmail.com>
2025-10-30 01:34:31 +0500

    chore: react scam

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "@tanstack/react-router";
 import { createRoot } from "react-dom/client";
+import { scan } from "react-scan";
 import { queryClient } from "~/lib/query-client";
 import { router } from "~/lib/router";
-import { reportWebVitals } from "./report-web-vitals";
+import { reportWebVitals } from "~/report-web-vitals";
+
+scan({
+  enabled: true,
+});
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
