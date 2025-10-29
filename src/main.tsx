bb89c17eb9f36aae458157b40e7df6b9ccfd92eb
MuhammadUsamaAwan <underusama@gmail.com>
2025-10-30 00:56:14 +0500

    chore: tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "@tanstack/react-router";
 import { createRoot } from "react-dom/client";
+import { queryClient } from "~/lib/query-client";
 import { router } from "~/lib/router";
 
-createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
+);
