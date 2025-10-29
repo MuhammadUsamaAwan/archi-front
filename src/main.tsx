d644dc1042c0706eb3237ddd70a6f89ead8a794a
MuhammadUsamaAwan <underusama@gmail.com>
2025-10-30 00:51:58 +0500

    chore: tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import { RouterProvider } from "@tanstack/react-router";
 import { createRoot } from "react-dom/client";
+import { router } from "~/lib/router";
 
-createRoot(document.getElementById("root")!).render(<h1>Hello World</h1>);
+createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
@@ -5,6 +6,10 @@ import tsConfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [
     tsConfigPaths(),
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
     react({
       babel: {
         plugins: [["babel-plugin-react-compiler"]],
